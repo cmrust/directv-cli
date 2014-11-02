d138b4ca80e31122dfa221261a4187d4eacc4f03
raptastics <crust4@gmail.com>
2014-11-02 14:39:57 -0600

    Renamed isEmpty fn and added findLocalSubnet fn

--- utils.js
+++ utils.js
@@ -9,8 +9,18 @@ module.exports.buildQueryString = function(path, qs) {
         }
     }
     return path;
-}
+};
 
-module.exports.isEmptyObject = function (obj) {
+module.exports.isEmpty = function (obj) {
     return (Object.keys(obj).length === 0) ? true : false;
-}
+};
+
+module.exports.findLocalSubnet = function (callback) {
+    require('dns').lookup(require('os').hostname(), function (err, address, fam) {
+        if (err) {
+            callback(new Error('Failed to find current local IP address: ' + err));
+        } else {
+            callback(null, address.split('.').slice(0,3).join('.') + '.');
+        }
+    });
+};
